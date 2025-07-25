8997adf539817ce2add5d639b493976e88331c8a
Alexandr V. <Alexandr.mersalies@gmail.com>
2025-07-25 23:01:48 +0500

    начал делать обработку сырых изображений

---
+++ processing_face.py
@@ -0,0 +1,20 @@
+import os
+from PIL import Image
+from facenet_pytorch import MTCNN
+from tqdm import tqdm
+
+import torch as th
+
+
+RAW_DIR = "raw_dataset"
+OUT_DIR = "processed_dataset"
+
+# Создаём выходную папку
+os.makedirs(OUT_DIR, exist_ok=True)
+
+# включаем использование cuda если оно есть
+device = 'cuda' if th.cuda.is_available() else 'cpu'
+
+
+mtcnn = MTCNN(image_size=160, margin=20, device=device)
+
