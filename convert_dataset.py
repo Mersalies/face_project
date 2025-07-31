1e17e0f1bb02d97749476c050d9df99708a38bb8
Alexandr V. <Alexandr.mersalies@gmail.com>
2025-07-31 22:46:24 +0500

    работа с эмбеддингами

--- convert_dataset.py
+++ convert_dataset.py
@@ -1,4 +1,4 @@
-from obrabotka_face import process_face_dataset
+from obrabotka_face import process_face_dataset, generation_embedding
 
 
 RAW_DIR_TRAIN = "archive/train"                
@@ -11,6 +11,10 @@ OUT_DIR_VAL = "processed_dataset/val"      # папка с обработаны�
 
 
 RAW_DIR_VER = "photo"
-OUR_DIR_VER = "photo_to_verifecation/embding_photo"
-process_face_dataset(raw_dir=RAW_DIR_VER, out_dir=OUR_DIR_VER)
+OUR_DIR_VER = "photo_to_verifecation/MyPhoto"
+# process_face_dataset(raw_dir=RAW_DIR_VER, out_dir=OUR_DIR_VER)
+
+RAW_DIR_EMD = "photo_to_verifecation/MyPhoto"
+OUT_DIR_EMD = "photo_to_verifecation/embding_photo"
+# generation_embedding(raw_dir=RAW_DIR_EMD, out_dir=OUT_DIR_EMD)
 
