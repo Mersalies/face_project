324bed98a4a9798c07f1647150ba5f9d75dbfb9b
Alexandr V. <Alexandr.mersalies@gmail.com>
2025-07-31 21:37:29 +0500

    Добавил обработку своих фотографий

--- convert_dataset.py
+++ convert_dataset.py
@@ -7,5 +7,10 @@ OUT_DIR_TRAIN = "processed_dataset/train"
 
 RAW_DIR_VAL = "archive/val"                # папка с оригинальными изображениями
 OUT_DIR_VAL = "processed_dataset/val"      # папка с обработаными изображениями
+# process_face_dataset(raw_dir= RAW_DIR_VAL, out_dir=OUT_DIR_VAL)
 
-process_face_dataset(raw_dir= RAW_DIR_VAL, out_dir=OUT_DIR_VAL)
+
+RAW_DIR_VER = "photo"
+OUR_DIR_VER = "photo_to_verifecation/embding_photo"
+process_face_dataset(raw_dir=RAW_DIR_VER, out_dir=OUR_DIR_VER)
+
