85881d54fbed862609e55073baa737f307847901
Alexandr V. <Alexandr.mersalies@gmail.com>
2025-08-03 17:56:10 +0500

    изменил структуру ошибки

--- training_model/loss.py
+++ training_model/loss.py
@@ -1,23 +1,16 @@
+# triplet/loss.py
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class TripletLoss(nn.Module):
-
     def __init__(self, margin=0.5):
-        super(TripletLoss,self).__init__()
+        super(TripletLoss, self).__init__()
         self.margin = margin
 
-    
-    def forward(self, anchor, positive,negative):
+    def forward(self, anchor, positive, negative):
+        distance_positive = F.pairwise_distance(anchor, positive, p=2)
+        distance_negative = F.pairwise_distance(anchor, negative, p=2)
 
-        # Расстояние междку векторами 
-        rastoynie_pos = F.pairwise_distance(anchor,positive, p=2)
-        rastoynie_neg = F.pairwise_distance(anchor,negative, p=2)
-
-        #Tripelet loss
-        loss = F.relu(rastoynie_pos - rastoynie_neg + self.margin)
-
-        return loss.mean()
-
+        losses = F.relu(distance_positive - distance_negative + self.margin)
+        return losses.mean()
